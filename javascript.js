0b0972b5fa42c9b8f549538ab847af58d70975e8
Taylir <tdl9716@gmail.com>
2024-10-16 19:14:12 -0400

    Remove HTML test and add JS test

--- javascript.js
+++ javascript.js
@@ -1,1 +1,7 @@
+const searchForm = document.querySelector("#search");
+const pokeSearch = document.querySelector(".btn-search");
 
+window.addEventListener("keyup", (e)=>{
+  if (e.key == "Enter") console.log(true);
+  if (e.key !== "Enter") console.log(false);
+});
