a5eb3672eeea345ce766babe17486b603ce78e46
Taylir <tdl9716@gmail.com>
2024-10-18 18:39:30 -0400

    Will cause merge issue to fix

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const searchForm = document.querySelector("#search");
 const pokeSearch = document.querySelector(".btn-search");
 
@@ -5,3 +6,6 @@ window.addEventListener("keyup", (e)=>{
   if (e.key == "Enter") console.log(true);
   if (e.key !== "Enter") console.log(false);
 });
+=======
+console.log("Hello I am connected correctly")
+>>>>>>> parent of d8686f3 (Add search bar and small css features)
